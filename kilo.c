8553c4346fe994f389206b80dbe30f7f40b89c46
Ananthakrishnan Balajee <ananthakrishnanbalajee@gmail.com>
2023-11-12 10:50:30 +0000

    Initial Commit - I've just started using Git for this project :)

---
+++ kilo.c
@@ -0,0 +1,83 @@
+/*** includes ***/
+#include <ctype.h>
+
+#include <errno.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <termios.h>
+
+#include <unistd.h>
+
+/*** data ***/
+struct termios orig_termios;
+
+/*** terminal ***/
+void die(const char* s) {perror(s); exit(1);} // error handling, perror() from stdio.h. perror() looks at the global errno variable and prints the error, as well as the string s to give context about where the error occurred
+// any non zero exit() value means failure. exit() comes from stdlib.h
+void disableRawMode()
+{
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) //The `TCSAFLUSH` argument specifies when to apply the change. see line 26
+    // because of this leftover input is no longer fed to the terminal. TCSAFLUSH
+    {
+        die("tcserattr");
+    }
+}
+
+void enableRawMode()
+{
+    struct termios raw; //create a termios structure called raw
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) //get terminal attributes into raw
+    {
+        die("tcgetattr");
+    }
+    atexit(disableRawMode); //atexit() comes from <stdlib.h>.
+    // We use it to register our disableRawMode() function to be called automatically 
+    //when the program exits, whether it exits by returning from main(), or by calling the exit() function. This way we can ensure we’ll leave the terminal attributes the way we found them when our program exits.
+    raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //ICRNL is ['I' input flag, 'CR' is carriage return, and 'NL' is newline. stops coversion from carriage return (Ctrl - M or Enter {ASCII 13}) to newline(Ctrl - J or ASCII 10) ]| turns off the Ctrl - S and Ctrl - Q control signals (XOFF and XON)
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // setting the echo flag off so that the terminal does not echo whatever one types. canonical mode is also flipped off. now we read byte by byte. we exit as soon as 'q' is pressed.
+    // .c_lflag field is for "local flags"
+    /*ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. 
+    We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111. 
+    We then bitwise-AND this value with the flags field, which forces the fourth bit in the flags field to become 0, 
+    and causes every other bit to retain its current value. 
+    Flipping bits like this is common in C.*/
+    raw.c_oflag &= ~(OPOST); // turns off output processing. POST is post processing of output. look up on \n to \r\n and carriagr returns and newlines
+    raw.c_cflag |= (CS8); //character size 8 bits. this is a bit mask
+    raw.c_cc[VMIN] = 0; // minimum bytes before read() can return
+    raw.c_cc[VTIME] = 1; // time to wait before read() returns. time in 1/10 of a second
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) // set the terminal attributes to those mentioned in raw
+    /*The `TCSAFLUSH` argument specifies when to apply the change: 
+    in this case, it waits for all pending output to be written to the terminal,
+     and also discards any input that hasn’t been read.*/
+     {
+        die("tcsetattr");
+     }
+}
+
+/*** init ***/
+int main()
+{
+    enableRawMode();
+    while(1)
+    {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read"); // errno and EAGAIN from errno.h
+        if (iscntrl(c)) // comes from ctype.h and checks if the char is a control character (non-printable character) [ASCII 0-31 and 127 are control characters]
+        {
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+
+        if (c == 'q') break;
+        
+        
+    }
+    return 0;
+}
